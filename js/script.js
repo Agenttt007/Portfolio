e5cb00cb510c09606e984efa78027e60d2a67d0e
Агент007 <alex1035@mail.ru>
2025-09-11 18:59:17 +0300

    Добавляем функцию и практикуемся

--- js/script.js
+++ js/script.js
@@ -19,11 +19,11 @@ function User(name){
     this.isAdmin = false;
 }
 
-let user = new User('Вася');*/
+let user = new User('Вася');
 
 const arr = [1, 22, 333];
 
-/*for (let i = 0; i < arr.length; i++){
+for (let i = 0; i < arr.length; i++){
     console.log(arr[i]);
 }
 
@@ -31,11 +31,24 @@ for (let item of arr){
     console.log(item);
 }
 
-console.log(arr);*/
+console.log(arr);
 
 arr.forEach((item, index, array) => {
     console.log(item);
     console.log(index);
     console.log(array);
-});
+});*/
 
+const learning = (lang) => {
+    setTimeout(() => {
+        console.log('Получил важные данные с сервера')
+        console.log(`Начинаю учить ${lang}`);
+    }, 2000);
+}
+
+const blabla = (lessonNumber) => {
+    console.log(`Я прошёл ${lessonNumber}-й урок`);
+}
+
+learning('JavaScript');
+blabla(1);
