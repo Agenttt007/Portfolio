aefff0102721f486166152e35cc726571a8707e8
Агент007 <alex1035@mail.ru>
2025-09-19 20:20:03 +0300

    Добавляем файлы из архива и пробую

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+//const phoneinput = document.querySelector('input[type=phone]');
+/*const  box = document.querySelector('#box');
+        circles = document.querySelectorAll('.circle');
+        div = document.createElement('div');*/
+
+
+//console.log(box);
+//box.computedStyleMap.color = 'blue';
+//box.style.cssText = 'background-color: blue; color: red';
+
+/*circles.forEach(item => {
+    item.classList.toogle('after');
+});
+
+console.log(circles);*/
+
+const style = document.createElement('link');
+
+style.rel = 'stylesheet';
+style.href = 'css/new.css';
+
+console.log(style);
+
+document.head.append(style);
+
+style.remove();
+
