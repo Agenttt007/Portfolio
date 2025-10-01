8014077eac054c1e9e28e45736133ca2dae8d956
[Алексей007] <[alex1035@mail.ru]>
2025-10-01 11:04:17 +0300

    Поправил отступы и выравниваниее

--- js/script.js
+++ js/script.js
@@ -34,33 +34,3 @@ const skills = {
 const skillList = document.querySelector('dl.skill-list');
 
 skills.generateList(skillList);
-
-
-/*const phoneinput = document.querySelector('input[type=phone]');
-const  box = document.querySelector('#box');
-        circles = document.querySelectorAll('.circle');
-        div = document.createElement('div');*/
-
-
-//console.log(box);
-//box.computedStyleMap.color = 'blue';
-//box.style.cssText = 'background-color: blue; color: red';
-
-/*circles.forEach(item => {
-    item.classList.toogle('after');
-});
-
-console.log(circles);
-
-const style = document.createElement('link');
-
-style.rel = 'stylesheet';
-style.href = 'css/new.css';
-
-console.log(style);
-
-document.head.append(style);
-
-style.remove();*/
-
-
